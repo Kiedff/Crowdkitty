ee6aa9485863fa5a5c210025a478a4a2232f9403
Chae Cramb <chaecramb@gmail.com>
2015-12-04 15:36:49 +0000

    migrates current migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204114202) do
+ActiveRecord::Schema.define(version: 20151204153335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,56 @@ ActiveRecord::Schema.define(version: 20151204114202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "image"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pictures", ["project_id"], name: "index_pictures_on_project_id", using: :btree
+
+  create_table "pledges", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.integer  "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "pledges", ["project_id"], name: "index_pledges_on_project_id", using: :btree
+  add_index "pledges", ["user_id"], name: "index_pledges_on_user_id", using: :btree
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "target"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "user_id"
+    t.string   "location"
+    t.text     "summary"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "projects", ["category_id"], name: "index_projects_on_category_id", using: :btree
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
+
+  create_table "rewards", force: :cascade do |t|
+    t.integer  "project_id"
+    t.text     "description"
+    t.integer  "value"
+    t.integer  "quantity"
+    t.date     "delivery"
+    t.string   "ships_to"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "rewards", ["project_id"], name: "index_rewards_on_project_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,4 +95,10 @@ ActiveRecord::Schema.define(version: 20151204114202) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "pictures", "projects"
+  add_foreign_key "pledges", "projects"
+  add_foreign_key "pledges", "users"
+  add_foreign_key "projects", "categories"
+  add_foreign_key "projects", "users"
+  add_foreign_key "rewards", "projects"
 end
