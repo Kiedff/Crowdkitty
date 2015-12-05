d5828292347b774599f85026e178100921f06517
Chae Cramb <chaecramb@gmail.com>
2015-12-05 14:34:55 +0000

    defines role abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,19 @@ class Ability
 
   def initialize(user)
     def initialize(user)
+      alias_action :create, :update, :destroy, :to => :cud
+
       user ||= User.new
       if user.role? :admin
         can :manage, :all
+      elsif user.role? :user
+        can :read, :all
+        can :cud, Project, :user_id => user.id  
+        can :cud, Reward # needs resrictions added 
+        can :cud, Picture # needs resrictions added 
+        can :create, Pledge 
       else
         can :read, :all
-        can :create, :projects
       end
     end
   end
