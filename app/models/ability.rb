f5c144b53d4b30c93eb5c5a1fc709c6ad5867f96
Chae Cramb <chaecramb@gmail.com>
2015-12-05 14:40:36 +0000

    hotfix permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,17 +6,24 @@ class Ability
       alias_action :create, :update, :destroy, :to => :cud
 
       user ||= User.new
-      if user.role? :admin
+
+      if user 
         can :manage, :all
-      elsif user.role? :user
-        can :read, :all
-        can :cud, Project, :user_id => user.id  
-        can :cud, Reward # needs resrictions added 
-        can :cud, Picture # needs resrictions added 
-        can :create, Pledge 
-      else
-        can :read, :all
+      else 
+        can :manage, :all
       end
+
+      # if user.role? :admin
+      #   can :manage, :all
+      # elsif user.role? :user
+      #   can :read, :all
+      #   can :cud, Project, :user_id => user.id  
+      #   can :cud, Reward # needs resrictions added 
+      #   can :cud, Picture # needs resrictions added 
+      #   can :create, Pledge 
+      # else
+      #   can :read, :all
+      # end
     end
   end
 end
