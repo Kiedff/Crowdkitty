e1b9a0b6076a97312fd889878e5e9b2268497388
Keith Martin <user@Users-MacBook-Pro-22.local>
2015-12-05 11:53:59 +0000

    index, new, create and show actions added to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,31 @@
 class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+      project = Project.new(project_params)
+      project.user_id = current_user.id if current_user
+      if project.save
+        flash[:success] = "Project created! Good luck!"
+        redirect_to project
+      else
+        render 'new'
+      end
+  end
+  
   def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :user_id, :target, :end_date, :location, :summary, :category_id)
   end
 
 end
