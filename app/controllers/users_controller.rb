87a29caecc61b9c477b755b54058aa662ab153e7
Keith Martin <user@Users-MacBook-Pro-22.local>
2015-12-05 15:18:49 +0000

    modified user controller so that index page displays

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,40 +2,24 @@ class UsersController < ApplicationController
   
  load_and_authorize_resource
 
+
  def index
-      @users = User.where(id: current_user.id)#User.all
-    end
+   @users = User.all
+ end
 
-    def new
-      @user = User.new
-    end
+ def show
+   @user = User.find(params[:id])
+ end
 
-    def create
-      User.create(user_params)
-      redirect_to(users_path)
-    end
+ def edit
+   @user = current_user
+ end
 
-    def show
-      #@user = User.where(id: current_user.id)
-      @user = User.find(current_user.id)
-    end
-
-    def destroy
-      user = User.find(params[:id])
-      user.destroy
-      redirect_to(users_path)
-    end
-
-    def update
-      user = User.find(params[:id])
-      user.update(user_params)
-      redirect_to(users_path)
-    end
-
-    def edit
-      @user= User.find(params[:id])
-    end
-
+ def update
+  user = User.find(params[:id])
+  user.update(user_params)
+  redirect_to(user)
+ end
     private
     def user_params
       params.require(:user).permit(:name, :address1, :address2, :email, :username, :last_name, :city, :postcode, :user_image)
