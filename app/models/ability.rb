921552797c0271731aadbf2f58edb5f0c9c04467
Chae Cramb <chaecramb@gmail.com>
2015-12-06 10:43:21 +0000

    fix cancan bug

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,24 +2,22 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    def initialize(user)
-      alias_action :create, :update, :destroy, :to => :cud
+    alias_action :create, :update, :destroy, :to => :cud
 
-      user ||= User.new
+    user ||= User.new
 
-      if user.role? :admin
-        can :manage, :all
-      elsif user.role? :user
-        can :cud, Project, :user_id => user.id  
-        can :cud, Reward # needs restrictions added 
-        can :cud, Picture # needs restrictions added 
-        can :create, Pledge 
-        can :update, User
-        can :read, :all
-        can :welcome, :Home
-      else
-        can :read, :all
-      end
+    if user.role? :admin
+      can :manage, :all
+    elsif user.role? :user
+      can :cud, Project, :user_id => user.id  
+      can :cud, Reward # needs restrictions added 
+      can :cud, Picture # needs restrictions added 
+      can :create, Pledge 
+      can :update, User
+      can :read, :all
+      can :welcome, :Home
+    else
+      can :read, :all
     end
   end
 end
