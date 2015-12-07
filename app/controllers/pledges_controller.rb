992c650067d3bf610f784093bd7fcdb84632b74d
Keith Martin <user@Users-MacBook-Pro-22.local>
2015-12-07 11:24:50 +0000

    can now pledge any value without claiming reward

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,15 +1,21 @@
 class PledgesController < ApplicationController
 
-  def create
-    
-    pledge = Pledge.new(pledge_params.merge( user_id: current_user.id, reward_id:params[:reward_id] ))
+  def new
+    @pledge = Pledge.new
+    @project = Project.find(params[:project_id])
+  end
 
+  def create
+    pledge = Pledge.new(pledge_params.merge( user_id: current_user.id, reward_id:params[:reward_id], project_id: params[:project_id] ))
+    if pledge.reward_id
     reward_project_id = Reward.where(id: pledge.reward_id).first.project_id
     pledge.project_id = reward_project_id
+  end
     
     if pledge.save
       flash[:success] = "Pledged successfully!"
-      redirect_to :back
+      project = Project.find(params[:project_id])
+      redirect_to project
     else
       render 'new'
     end
