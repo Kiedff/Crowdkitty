61cdae37c723498d6f375992d69968648f9759d2
Keith Martin <user@Users-MacBook-Pro-22.local>
2015-12-07 14:30:03 +0000

    pledges can only be cancelled within first 24 hours

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,5 +1,12 @@
 class Pledge < ActiveRecord::Base
+  before_destroy :destroyable?
+
   belongs_to :user
   belongs_to :project
   belongs_to :reward
+
+  def destroyable?
+    created_at < 1.day.ago
+  end
 end
+
