584c4d6273cfbd6385f7eb51b5826d9719d30a12
Chae Cramb <chaecramb@gmail.com>
2015-12-08 13:42:16 +0000

    adds html for homepage filters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def welcome
+    @categories = Category.order(:name)
     @most_popular = Project.sort_by_pledges(Project.all).first(3)
     @finishing_soon = Project.sort_by_finishing_soon(Project.all).first(3)
   end
